bb574098dbc7fd258cf8026d91812e62ff441a6d
Remek-P <remek.piotrowski@wp.pl>
2024-05-10 18:12:49 +0200

    no trimming

--- components/output/output.js
+++ components/output/output.js
@@ -24,7 +24,7 @@ function Output({ excelFile }) {
   return (
       <>
         <input type="text"
-               value={inputValue} onChange={(e) => setInputValue(e.target.value.toString().trim())}
+               value={inputValue} onChange={(e) => setInputValue(e.target.value.toString())}
                placeholder="Please type to filter a person"
         />
         <div className={classes.container1}>
--- components/search/search.js
+++ components/search/search.js
@@ -1,7 +1,7 @@
 function Search({ inputValue, setInputValue }) {
   return (
       <input type="text"
-             value={inputValue} onChange={(e) => setInputValue(e.target.value.toString().trim())}
+             value={inputValue} onChange={(e) => setInputValue(e.target.value.toString())}
              placeholder="Please type to filter a person"
       />
   );
