7d2f456bfc94d23881038e7207288ba123d3dc4c
Remek-P <remek.piotrowski@wp.pl>
2024-06-04 21:33:50 +0200

    search.js: added delete button with tooltip
    show-hide-chart-button.js:
    added tooltips to buttons

--- components/output/section/show-hide-chart-button/show-hide-chart-button.js
+++ components/output/section/show-hide-chart-button/show-hide-chart-button.js
@@ -1,13 +1,15 @@
-import {Button} from "@carbon/react";
+import { Button, Tooltip } from "@carbon/react";
 
 function ShowHideChartButton({ onClick, children }) {
   return (
+      <Tooltip align="top" description="toggle chart" closeOnActivation={true}>
       <Button size="sm"
               kind="ghost"
               onClick={onClick}
       >
         { children }
       </Button>
+      </Tooltip>
   );
 }
 
--- components/search/search.js
+++ components/search/search.js
@@ -1,36 +1,46 @@
-import classes from "./search.module.scss";
+import { Button, Tooltip } from "@carbon/react";
 
+import classes from "./search.module.scss";
 
 function Search({ inputValue, setInputValue, searchRef, id="search" }) {
 
   const handleTyping = (e) => {
-    // const input = e.target.value.toString();
-    // const check = input.match(/^[0-9]*$/);
-    // if (check) setInputValue(input)
-
     setInputValue(e.target.value.toString());
   }
 
+  const handleDeleteButton = () => {
+    setInputValue("");
+    searchRef.current.focus();
+  }
+
   return (
-      <div className={classes.searchContainer} >
-          <svg className={classes.searchIcon}
-               xmlns="http://www.w3.org/2000/svg"
-               id="icon"
-               viewBox="0 0 32 32">
-            <path xmlns="http://www.w3.org/2000/svg"
-                  d="M29,27.5859l-7.5521-7.5521a11.0177,11.0177,0,1,0-1.4141,1.4141L27.5859,29ZM4,13a9,9,0,1,1,9,9A9.01,9.01,0,0,1,4,13Z"/>
-          </svg>
-          <input id={id}
-                 name={id}
-                 list={id}
-                 type="search"
-                 title="Numbers only"
-                 value={inputValue}
-                 placeholder="Please type to filter a person"
-                 className={classes.search}
-                 onChange={(e) => handleTyping(e)}
-                 ref={searchRef}
+      <div className={classes.searchContainer}>
+        <svg className={classes.searchIcon}
+             xmlns="http://www.w3.org/2000/svg"
+             id="icon"
+             viewBox="0 0 32 32">
+          <path xmlns="http://www.w3.org/2000/svg"
+                d="M29,27.5859l-7.5521-7.5521a11.0177,11.0177,0,1,0-1.4141,1.4141L27.5859,29ZM4,13a9,9,0,1,1,9,9A9.01,9.01,0,0,1,4,13Z"/>
+        </svg>
+        <input id={id}
+               name={id}
+               list={id}
+               type="search"
+               title="Numbers only"
+               value={inputValue}
+               placeholder="Please type to filter a person"
+               className={classes.search}
+               onChange={(e) => handleTyping(e)}
+               ref={searchRef}
+        />
+        <Tooltip align="bottom" description="clear">
+          <Button kind="ghost"
+                  onClick={handleDeleteButton}
+                  size="sm"
+                  children="x"
           />
+
+        </Tooltip>
       </div>
 
   );
