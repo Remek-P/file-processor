400e4cf48f36a34a920710cf089ede9461962f00
Remek-P <remek.piotrowski@wp.pl>
2024-06-06 21:41:54 +0200

    autocomplete off

--- components/search/search.js
+++ components/search/search.js
@@ -23,6 +23,7 @@ function Search({ inputValue, setInputValue, searchRef, id="search" }) {
                 d="M29,27.5859l-7.5521-7.5521a11.0177,11.0177,0,1,0-1.4141,1.4141L27.5859,29ZM4,13a9,9,0,1,1,9,9A9.01,9.01,0,0,1,4,13Z"/>
         </svg>
 
+        <label htmlFor={id}>Search</label>
         <input id={id}
                name={id}
                list={id}
