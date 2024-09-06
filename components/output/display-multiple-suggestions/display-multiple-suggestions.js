a65354be51dc0f6ba94e9761ef81670caaa20700
Remek-P <remek.piotrowski@wp.pl>
2024-09-06 17:03:09 +0200

    useWindowSize.js - custom hook to get window size
    display-multiple-suggestions.js - changed to table view and implemented windowing (react-window)
    
    deleted unnecessary components

--- components/output/display-multiple-suggestions/display-multiple-suggestions.js
+++ components/output/display-multiple-suggestions/display-multiple-suggestions.js
@@ -1,12 +1,15 @@
-import {useMemo, useRef, useState} from "react";
+import {createContext, forwardRef, useEffect, useMemo, useRef, useState} from "react";
 
 import TexTile from "@/components/tile-type/text-tile/texTile";
-import SearchSuggestions from "@/components/search/suggestions/search-suggestions";
+
+import { FixedSizeList as List } from "react-window";
 
 import { compareValues } from "@/utils/sortUtils";
 
+import { Tile } from "@carbon/react";
+
 import classes from "@/components/output/output.module.scss";
-import {Tile} from "@carbon/react";
+import useWindowDimensions from "@/utils/useWindowSize";
 
 function DisplayMultipleSuggestions({
                                       IDIndex,
@@ -19,6 +22,9 @@ function DisplayMultipleSuggestions({
                                     }) {
 
   // const [active, setActive] = useState(IDIndex);
+
+  const {width, height} = useWindowDimensions();
+  const [isLoading, setIsLoading] = useState(false)
 
   const reducePerformanceStrain = inputValue.length < 2;
 
@@ -50,14 +56,84 @@ function DisplayMultipleSuggestions({
 
   const handleSort = (event) => {
     //TODO: each time the icon is pressed, the sorting should start from ascending and not oscillating
-
+    setIsLoading(true);
     indexToSort.current = event.currentTarget.cellIndex;
     setSearchSuggestionsOrder(prevState => !prevState);
+
     // if (+event.currentTarget.dataset.index === index) {
     //   setShowIcon(index);
     //   setActive(index);
     // }
   }
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [sortedSuggestions]);
+
+  const StickyListContext = createContext();
+  StickyListContext.displayName = "StickyListContext";
+
+  const ItemWrapper = ({ data, index, style }) => {
+    const { ItemRenderer, stickyIndices } = data;
+    if (stickyIndices && stickyIndices.includes(index)) {
+      return null;
+    }
+    return <ItemRenderer index={index} style={style} />;
+  };
+
+  const Row = ({index, style}) => {
+    const row = sortedSuggestions[index];
+    return (
+        <tr className="row" style={style}>
+          {row.map((value, colIndex) => (
+              <td key={colIndex} data-value={row[IDIndex]} onClick={pickSearchedOutput}
+                  className={classes.searchSuggestionTableRow}>
+                <Tile>
+                  {value}
+                </Tile>
+              </td>
+          ))}
+        </tr>
+    )
+  };
+
+  const StickyRow = ({ index, style }) => (
+      <tr className="sticky" style={style}>
+        {labelDataArray.map((label, index) => (
+            <th key={index} onClick={handleSort} className={classes.searchSuggestionTableHeader}>
+              <Tile>
+                {label}
+              </Tile>
+            </th>
+        ))}
+      </tr>
+  );
+
+  const innerElementType = forwardRef(({ children, ...rest }, ref) => (
+      <StickyListContext.Consumer>
+        {({ stickyIndices }) => (
+            <table ref={ref} {...rest}>
+              {stickyIndices.map((index) => (
+                  <StickyRow
+                      index={index}
+                      key={index}
+                      style={{ top: index * 35, left: 0, width: "100%", height: 10 }}
+                  />
+              ))}
+
+              <tbody>{children}</tbody>
+            </table>
+        )}
+      </StickyListContext.Consumer>
+  ));
+
+  const StickyList = ({ children, stickyIndices, ...rest }) => (
+      <StickyListContext.Provider value={{ ItemRenderer: children, stickyIndices }}>
+        <List itemData={{ ItemRenderer: children, stickyIndices }} {...rest}>
+          {ItemWrapper}
+        </List>
+      </StickyListContext.Provider>
+  );
 
   return (
       <section>
@@ -66,58 +142,19 @@ function DisplayMultipleSuggestions({
             && <TexTile text={"Please type at least 2 characters to display search results"}/>
         }
 
-        {/*<ul className={classes.searchContainer}>*/}
-        {/*  {!reducePerformanceStrain && labelDataArray.map((label, index) =>*/}
-        {/*      <SearchSuggestions key={index}*/}
-        {/*                         label={label}*/}
-        {/*                         // active={active}*/}
-        {/*                         // setActive={setActive}*/}
-        {/*                         index={index}*/}
-        {/*                         IDIndex={IDIndex}*/}
-        {/*                         indexToSort={indexToSort}*/}
-        {/*                         sortedSuggestions={sortedSuggestions}*/}
-        {/*                         searchSuggestionsOrder={searchSuggestionsOrder}*/}
-        {/*                         setSearchSuggestionsOrder={setSearchSuggestionsOrder}*/}
-        {/*                         pickSearchedOutput={pickSearchedOutput}*/}
-        {/*      />*/}
-        {/*  )}*/}
-        {/*  {!reducePerformanceStrain && labelDataArray.map((label, index) =>*/}
-        {/*      <li key={index}>*/}
-        {/*        {label}*/}
-        {/*      </li>*/}
-        {/*  )}*/}
-        {/*</ul>*/}
-
-        <table>
-          <thead>
-          <tr>
-            {labelDataArray.map((label, index) => (
-                <th key={index} onClick={handleSort}> {/* Assuming columns correspond to index */}
-                  <Tile>
-                    {label}
-                  </Tile>
-                </th>
-
-            ))}
-          </tr>
-          </thead>
-          <tbody>
-          {sortedSuggestions.map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                {row.map((value, colIndex) => (
-                    <td data-value={row[IDIndex]} key={colIndex} onClick={pickSearchedOutput}>
-                      <Tile>
-                        {value}
-                      </Tile>
-                    </td>
-                ))}
-              </tr>
-          ))}
-          </tbody>
-        </table>
+        <StickyList
+            height={height}
+            innerElementType={innerElementType}
+            itemCount={sortedSuggestions.length}
+            itemSize={50}
+            stickyIndices={[0]}
+            width={width}
+        >
+          {Row}
+        </StickyList>
 
       </section>
   );
 }
 
-          export default DisplayMultipleSuggestions;
+export default DisplayMultipleSuggestions;
