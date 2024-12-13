08d7b14a884fb6a82be21bfe1af9feff5cffde81
Remek-P <remek.piotrowski@wp.pl>
2024-12-13 12:32:54 +0100

    fixed the worker not sorting due to old constant name.

--- public/sortWorker.js
+++ public/sortWorker.js
@@ -1,10 +1,10 @@
 import { compareValues } from "@/utils/sortUtils";
 
 self.onmessage = function (event) {
-  const { searchSuggestionsOrder, searchUsers, indexToSort } = event.data;
+  const { searchSuggestionsOrder, searchRecords, indexToSort } = event.data;
 
   if (searchSuggestionsOrder || searchSuggestionsOrder === false) {
-    const sorted = [...searchUsers].sort((a, b) => compareValues(
+    const sorted = [...searchRecords].sort((a, b) => compareValues(
             a[indexToSort.current],
             b[indexToSort.current],
             searchSuggestionsOrder
