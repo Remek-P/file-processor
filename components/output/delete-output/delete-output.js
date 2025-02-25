c00af08d1a512d7f7ab7eef07ab2f1b828d29ae7
Remek-P <remek.piotrowski@wp.pl>
2025-02-25 15:01:01 +0100

    Deleting last output cleans the excludedArray
    refactoring

--- components/output/delete-output/delete-output.js
+++ components/output/delete-output/delete-output.js
@@ -1,6 +1,6 @@
-import {useContext, useState} from "react";
+import { useContext, useState } from "react";
 
-import {NumberOfOutputsContext} from "@/context/global-context";
+import { ExcludedDataGlobalContext, NumberOfOutputsContext } from "@/context/global-context";
 
 import { CloseFilled, CloseOutline } from "@carbon/react/icons";
 import { IconButton } from "@carbon/react";
@@ -10,37 +10,46 @@ import classes from "../output.module.scss";
 function DeleteOutput({ outputId, setUserQuery }) {
 
   const [ numberOfOutputs, setNumberOfOutputs ] = useContext(NumberOfOutputsContext);
+  const [ , setExcludedArray ] = useContext(ExcludedDataGlobalContext);
 
-  const [hover, setHover] = useState(false);
+  const [ hover, setHover ] = useState(false);
 
-  const handleMouseEnter = () => {setHover(true)}
-  const handleMouseLeave = () => {setHover(false)}
-  const handleOnFocus = () => {setHover(true)}
-  const handleOnBlur = () => {setHover(false)}
-  
+  const handleMouseEnter = () => setHover(true);
+  const handleMouseLeave = () => setHover(false);
+  const handleOnFocus = () => setHover(true);
+  const handleOnBlur = () => setHover(false);
+
   const handleDelete = () => {
-    // setNumberOfOutputs([...numberOfOutputs].filter(output => output.id !== outputId));
-    if (numberOfOutputs.length <= 1) setNumberOfOutputs([{ id: Date.now() }])
-    else setNumberOfOutputs([...numberOfOutputs].filter(output => output.id !== outputId));
+    if (numberOfOutputs.length <= 1) {
+      setNumberOfOutputs([ { id: Date.now() } ])
+      setExcludedArray([])
+    }
+    else setNumberOfOutputs(
+        [ ...numberOfOutputs ].filter(output => output.id !== outputId)
+    );
     setUserQuery("")
   }
 
   return (
-        <div key={outputId}
-             className={classes.outputDeleteOutput}
-             onMouseEnter={handleMouseEnter}
-             onMouseLeave={handleMouseLeave}
-             onFocus={handleOnFocus}
-             onBlur={handleOnBlur}>
+      <div key={ outputId }
+           className={ classes.outputDeleteOutput }
+           onMouseEnter={ handleMouseEnter }
+           onMouseLeave={ handleMouseLeave }
+           onFocus={ handleOnFocus }
+           onBlur={ handleOnBlur }>
 
-          <IconButton kind="ghost" label="Delete" align="bottom" onClick={handleDelete.bind(outputId)}>
-            {!hover
-                ? <CloseOutline aria-label="Delete" />
-                : <CloseFilled aria-label="Delete" />
-            }
-          </IconButton>
+        <IconButton kind="ghost"
+                    label="Delete"
+                    align="bottom"
+                    onClick={ () => handleDelete(outputId) }
+        >
+          { !hover
+              ? <CloseOutline aria-label="Delete" />
+              : <CloseFilled aria-label="Delete" />
+          }
+        </IconButton>
 
-        </div>
+      </div>
 
   );
 }
